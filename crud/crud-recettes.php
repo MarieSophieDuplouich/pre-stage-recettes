a9c49cf61612dca0fdfadfb102a9d83dc4b427f0
Marie-Sophie Duplouich <marie-sophieduplouich@Marie-Sophies-MacBook-Pro.local>
2025-08-04 15:20:53 +0200

    crud-recettes.php commencé

--- crud/crud-recettes.php
+++ crud/crud-recettes.php
@@ -1,9 +1,46 @@
-<?php function connect_database(): PDO
-{
-    $database = new PDO("mysql:host=127.0.0.1;dbname=app-database", "root", "root");
-    return $database;
-}
+<?php session_start();
+require_once "bdd-crud-recettes.php";
 ?>
+<?php
+// Test auth
+if (isset($_SESSION["id_user"]) == false) {
+    header("Location: login.php");
+    exit();
+}
+
+// Ajout d'une recette via le formulaire
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (isset($_SESSION['id_user']) && isset($_POST['title']) && isset($_POST['description'])) {
+        $id_user = $_SESSION['id_user'];
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+       add_recipe($id_user, $title, $description);
+    }
+
+    // Suppression d'une recette via le formulaire D
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+        delete_recipe($id);
+    } // lien cliquable get
+
+    // mettre à jour les données update U tâche
+
+    if (isset($_POST['id']) && isset($_POST['title']) && isset($_POST['description'])) {
+        $id = $_POST['id']; //// never change id 
+        $name = $_POST['title'];
+        $description = $_POST['description'];
+
+        update($id, $title, $description);
+    }
+}
+
+
+$recipess = trouver_recipe_par_id_user($_SESSION['id_user']); // c'est ça le read 
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
