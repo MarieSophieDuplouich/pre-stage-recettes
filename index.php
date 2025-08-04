0091304a99744dc62cb791c1d77a27d40030f1d7
Luz Rivera <luzrivera154@hotmail.com>
2025-08-04 14:57:05 +0200

    Style and navigation menu

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
+<?php
+session_start();
+//$recipes = getAllRecipes();
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="assets/style.css">
+    <title>Recipes</title>
 </head>
+
 <body>
-    <h1>recettes</h1>
+    <header>
+        <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="modification.php"></a>Create or Edit Recipe </li>
+            <li><a href="logout.php"></a>Log Out</li>
+        </ul>
+        </nav>
+    </header>
+    <div class="container">
+        <h1>Available recipes</h1>
+        <div class="container-recipes">
+            <?php foreach ($recipe as $recipe): ?>
+            <h2> <?= $recipe['title'] ?></h2>
+            <?php endforeach; ?>
+            <a href="recette-detailed.php">See more details</a>
+        </div>
+    </div>
 </body>
+
 </html>
