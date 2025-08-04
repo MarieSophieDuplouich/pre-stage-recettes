512ad7846fac84f65bd2c20e631ba5e35e1d9232
Marie-Sophie Duplouich <marie-sophieduplouich@Marie-Sophies-MacBook-Pro.local>
2025-08-04 15:14:36 +0200

    bdd crud recettes ajoutés le mettre en front

--- crud/crud-recettes.php
+++ crud/crud-recettes.php
@@ -1,3 +1,9 @@
+<?php function connect_database(): PDO
+{
+    $database = new PDO("mysql:host=127.0.0.1;dbname=app-database", "root", "root");
+    return $database;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +13,10 @@
     <link rel="stylesheet" href="../assets/crud-recettes.css">
 </head>
 <body>
-
+<!-- //luz a mis get all recipes getAllRecipes(); -->
     <h1>crud-recettes</h1>
+
+
    
 </body>
 </html>
