9951004dd787ae04eaf09a7e2b20a66df12e17fa
Javier <Javier@gmail.com>
2025-08-04 15:15:13 +0200

    Formulairer di conexion

--- login-logout/login.php
+++ login-logout/login.php
@@ -1,11 +1,31 @@
+<?php
+
+
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="conexion.css">
 </head>
 <body>
+    <div>
+        <form action="login.php" method="post">
+            <label for="login"></label>
+            <input type="text" name="username" id="username">
+            <label for="password"></label>
+            <input type="password" name="password" id="password">
+            <input type="button" value="=SEND">
+        </form>
+    </div>
     
 </body>
 </html>
