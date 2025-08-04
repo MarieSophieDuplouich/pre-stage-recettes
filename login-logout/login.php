655de8c4940846b8eddac5eb40176212192fc223
Javier <Javier@gmail.com>
2025-08-04 15:45:46 +0200

    Formulairer de registre con direccion

--- login-logout/login.php
+++ login-logout/login.php
@@ -14,17 +14,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="conexion.css">
+    <link rel="stylesheet" href="/assets/conexion.css">
 </head>
 <body>
     <div>
-        <form action="login.php" method="post">
-            <label for="login"></label>
+        
+        <form action="crud-user.php" method="post">
+            <label for="login"></label><br>
             <input type="text" name="username" id="username">
-            <label for="password"></label>
-            <input type="password" name="password" id="password">
-            <input type="button" value="=SEND">
+            <label for="password"></label><br>
+            <input type="password" name="password" id="password"><br>
+            <input type="button" value="Log in">
         </form>
+        <p>Don't have an account <a href="/login-logout/subscribe.php">Sing up here"</a></p>
     </div>
     
 </body>
